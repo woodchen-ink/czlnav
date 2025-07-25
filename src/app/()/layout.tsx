f266fa71abca04b5a851047477793003f0b9a937
wood chen <wood@czl.net>
2025-07-25 17:19:04 +0800

    feat: 添加网站分析脚本以跟踪网站访问量

--- src/app/()/layout.tsx
+++ src/app/()/layout.tsx
@@ -14,6 +14,11 @@ export default async function SiteLayout({
     <div className="flex flex-col min-h-screen">
       <Navbar siteName={settings.siteName || "CZL Nav"} />
       <main className="flex-grow">{children}</main>
+      <script
+        defer
+        src="https://analytics.czl.net/script.js"
+        data-website-id="5703b793-bb32-42df-9bd5-37a43c78f399"
+      ></script>
       <Footer
         siteDescription={settings.siteDescription}
         statisticsCode={settings.statisticsCode}
