3b94dee096e549a09ea80c18d49f69c9c79f5ef8
wood chen <wood@czl.net>
2026-04-08 21:03:01 +0800

    feat: 新增分类页面布局

---
+++ frontend/src/app/c/layout.tsx
@@ -0,0 +1,10 @@
+import "../globals.css";
+import PublicSiteShell from "@/components/PublicSiteShell";
+
+export default function CategoryLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <PublicSiteShell>{children}</PublicSiteShell>;
+}
